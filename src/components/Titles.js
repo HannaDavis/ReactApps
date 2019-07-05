4325456f126da4907b79b3043aa311d95cfdcbd8
Hanna Davis <hanna.davis.mail@gmail.com>
2019-07-05 18:01:20 -0400

    Create Weather App
    Weather app accepts city and country and
    displayes weather details retrieved from api call response
    to OpenWeather https://openweathermap.org/

---
+++ src/components/Titles.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Titles = () => (
+	<div>
+		<h1>Weather Finder</h1>
+		<p>Find out temperatures, conditions and more</p>
+	</div>);
+
+export default Titles;
